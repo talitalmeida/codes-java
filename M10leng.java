62572f3f12a25fa06c73f56f850eb01db89572d9
Talita Almeida <86754155+talitalmeida@users.noreply.github.com>
2023-01-10 14:46:10 -0300

    Módulo 10 - Length e Boolean

---
+++ M10leng.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class M10leng {
+
+	// Modulo 10, length
+
+	public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		String chave = leitura.next();
+		String digitoVerificador = mod10(chave);
+		System.out.println(digitoVerificador);
+
+	}
+
+	private static String mod10(String chave) {
+		int digito = 0;
+		int soma = 0;
+		int mult = 3;
+		boolean testador = false;
+
+		for (int i = 0; i < chave.length(); i++) {
+			int d = Integer.valueOf(chave.substring(i, i + 1));
+
+			if (d > 0) {
+				testador = true;
+			}
+			if (testador) {
+				int pond = d * mult;
+				soma += pond;
+
+				mult = mult == 3 ? 1 : 3;
+			}
+		}
+
+		digito = 10 - (soma % 10);
+		if (digito == 10)
+			digito = 0;
+
+		return String.valueOf(digito);
+	}
+
+}
